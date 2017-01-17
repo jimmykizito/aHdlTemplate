db1877e276cff2312d05466d49f95242c913b242
Jimmy Kizito <jkizito@gmail.com>
2017-01-17 23:07:54 +0200

    Updated template for top level VHDL file.

---
+++ hdl/vhdl/top___proj__.vhd
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity top__proj__ is
+  -- Port declarations
+  -- port (
+    -- p_i_NAME: in|inout|out std_logic|std_logic_vector();
+  -- );
+end top__proj__;
+
+architecture arch_top__proj__ of top__proj__ is
+
+  -- Submodule instantiation
+  -- component submodule_name
+    -- generic (
+      -- g_PARAM: type := DEFAULT_VALUE
+    -- );
+    -- port (
+      -- p_i_SUB_NAME: in|inout|out type;
+    -- );
+  -- end component;
+
+begin
+
+  -- i_submodule_name: submodule_name
+    -- generic map (
+      -- g_PARAM => VALUE
+    -- )
+    -- port map (
+      -- p_i_SUB_NAME => port|signal,
+    -- );
+
+end arch_top__proj__;
+
