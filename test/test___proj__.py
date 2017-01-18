ddf57872087d7436ce671a24f333cee5a079326f
Jimmy Kizito <jkizito@gmail.com>
2017-01-18 22:34:22 +0200

    Updated Python template for cocotb test.

---
+++ test/test___proj__.py
@@ -0,0 +1,14 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, RisingEdge
+from cocotb.result import TestFailure, TestSuccess
+
+@cocotb.test()
+def test__proj__(dut):
+    """
+    <Test description>
+    """
+    dut._log.info("Test beginning.")
+
+    dut._log.info("Test ending.")
+
