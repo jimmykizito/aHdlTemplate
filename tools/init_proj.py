52a6298f8a2314c50a31a9f3e06594f24a0b9cab
Jimmy Kizito <jkizito@gmail.com>
2017-04-06 22:22:24 +0200

    Added functionality to project initialisation script.
    - Script takes "project name" (space separated words) as primary argument.
    - Replaces placeholder text with project name in project files.
    - Renames project directory according to project name.
    - Prints status to standard output.

--- tools/init_proj.py
+++ tools/init_proj.py
@@ -6,30 +6,94 @@ Initialise HDL project.
 import argparse
 import os
 import shutil
+import sys
 
-if __name__ == "__main__":
+# TODO: Refactor to use InitProj class.
+
+def init_proj_fmt_name(proj_arg):
+    """
+    Format project name argument such that:
+    - It is all lower case.
+    - Words are separated by underscores.
+    """
+    proj_name = '_'.join(proj_arg.lower().split())
+    return proj_name
+
+def init_proj_fmt_dir(proj_name):
+    """
+    Format project name argument such that:
+    - It is CamelCase.
+    - It is prepended by the letter 'a'.
+    """
+    proj_dir = 'a' + ''.join([word.title() for word in proj_name.split('_')])
+    return proj_dir
+
+if __name__ == '__main__':
 
     # Parse arguments.
-    DIR_DEFAULT = 'aHdlTemplate.git'
+    PROJ_DIR_DEFAULT = 'aHdlTemplate.git'
     parser = argparse.ArgumentParser(description='Initialise HDL project.')
-    parser.add_argument('proj', nargs=1, help='Project name.')
-    parser.add_argument('--dir_orig', '-d', default=DIR_DEFAULT, nargs=1,
+    parser.add_argument('proj_name', help='Project name.')
+    parser.add_argument('--dir_orig', '-d', default=PROJ_DIR_DEFAULT,
                         help='Original project directory name. [default:%s]' %
-                        (DIR_DEFAULT))
+                        (PROJ_DIR_DEFAULT))
     args = parser.parse_args()
-    print(args)
 
     # Delete .git directory after determining project path.
     path_script = os.path.dirname(os.path.realpath(__file__))
     path_proj = path_script
     while args.dir_orig != os.path.basename(path_proj):
         path_proj = os.path.split(path_proj)[0]
+        if os.path.split(path_proj)[1] == '':
+            print 'Unable to find dir(%s)' % (args.dir_orig)
+            sys.exit(-1)
     path_git = os.path.join(path_proj, '.git')
     if os.path.isdir(path_git):
-        print("shutil.rmtree(path_git) %s" % (path_git))
+        print 'Removing git directory: %s' % (path_git)
         shutil.rmtree(path_git)
 
-    # Initialise project files
+    # Format project name and project directory name.
+    proj_name = init_proj_fmt_name(args.proj_name)
+    print 'Project name (%s) derived from script arg (%s).' \
+        % (proj_name, args.proj_name)
+    proj_dir = init_proj_fmt_dir(proj_name)
+    print 'Project directory (%s) derived from project name (%s).' \
+        % (proj_dir, proj_name)
 
-    # Initialise project file names
+    # Initialise project files.
+    # Constant list of prject files to modify.
+    # Each row it the path of the file relative to the project root directory.
+    PROJ_FILES = [
+        ['hdl', 'verilog', 'top___proj__.v'],
+        ['hdl', 'vhdl', 'top___proj__.vhd'],
+        ['test', 'test___proj__.py'],
+        ['test', 'verilog', 'Makefile'],
+        ['test', 'vhdl', 'Makefile']
+    ]
+    PROJ_NAME_DEFAULT = '__proj__'
+
+    for proj_file in PROJ_FILES:
+        in_file = os.path.join(path_proj, *proj_file)
+        print 'Initialising (%s)' % (in_file)
+        # Create a temporary file for project files that will not be renamed.
+        if PROJ_NAME_DEFAULT in in_file:
+            out_file = in_file.replace(PROJ_NAME_DEFAULT, proj_name)
+        else:
+            out_file = in_file + '.tmp'
+        with open(in_file, 'r') as file_in, open(out_file, 'w') as file_out:
+            for line_in in file_in:
+                line_out = line_in.replace(PROJ_NAME_DEFAULT, proj_name)
+                file_out.write(line_out)
+        if PROJ_NAME_DEFAULT in in_file:
+            os.remove(in_file)
+        else:
+            os.rename(out_file, in_file)
+
+    # Rename project directory.
+    proj_dir_old = path_proj
+    proj_dir_new = path_proj.replace(args.dir_orig, proj_dir)
+    print 'Project directory (%s) renamed as (%s).' \
+        % (proj_dir_old, proj_dir_new)
+    #Python 3 ? os.renames(proj_dir_old, proj_dir_new)
+    shutil.move(proj_dir_old, proj_dir_new)
 
